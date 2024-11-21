3fff8bde77f1201d0a58ad59d5801113f640867e
youtubeparty <122765225+youtubeparty@users.noreply.github.com>
2024-11-21 17:08:40 -0500

    push data aadded

--- sketch.js
+++ sketch.js
@@ -174,6 +174,16 @@ function receiveForm(event){
 	
 }
 
+async function pushData(paparazzo, celebrity){
+    const url = "https://upmfm7yzu2.execute-api.us-east-1.amazonaws.com/publish_to_birthdae" + new URLSearchParams({
+        paparazzo: paparazzo,
+        celebrity: celebrity,
+    });
+    const response = await fetch(url, {
+        method: "POST"
+    })
+}
+
 
 
 function saveCanvasToRemoteServer() {
