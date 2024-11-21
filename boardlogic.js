caa83cdce7dd89e51371e4c40ee60ec40c279d0c
youtubeparty <122765225+youtubeparty@users.noreply.github.com>
2024-11-21 17:37:43 -0500

    fix

--- boardlogic.js
+++ boardlogic.js
@@ -42,7 +42,7 @@ window.onload = function(){
 
 async function getData(){
     const response = await fetch('https://h0fkejytf5.execute-api.us-east-1.amazonaws.com/get_birthdae_rankings');
-    const data = response.json();
+    const data = await response.json();
     return data;
 }
 
